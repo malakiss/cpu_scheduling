5da0725287f79c188e2cc4379fd72af825458a8b
nourismail <nourismaill076@gmail.com>
2024-12-18 01:28:46 +0200

    rr

--- main.cpp
+++ main.cpp
@@ -58,6 +58,133 @@ void fcfs(Process p[], int num_processes) {
     }
 
 }
+void print_trace(Process p[], int num_processes, int timeline_length, const string& method, const vector<char>& timeline) {
+    // Print header for the timeline
+    cout << method << "  ";
+    for (int t = 0; t < 21; ++t) {
+        cout << t % 10 << " ";  // Time units
+    }
+    cout << endl;
+    cout << "------------------------------------------------" << endl;
+
+    // Print the timeline for each process
+    for (int i = 0; i < num_processes; ++i) {
+        cout << p[i].name << "    |";
+        for (int t = 0; t < timeline_length; ++t) {
+            // Mark the process as executing at the respective time slice
+            if (timeline[t] == p[i].name) {
+                cout << "*|";  // Execution time
+            } else if (p[i].arrival_time <= t && t< p[i].start_time  ) {
+                cout << ".|";  // Waiting time, process is in the queue but not executed yet
+            } else {
+                cout << " |";  // Idle time
+            }
+        }
+        cout << endl;
+    }
+    cout << "------------------------------------------------" << endl;
+}
+
+void round_robin(Process p[],int process_time, int num_processes, int quantum) {
+    queue<Process*> process_queue;  // Queue to hold processes
+    int current_time = 0;           // Current time in the schedule
+    int completed_count = 0; 
+    std::vector<char> timeline(process_time, ' ');       // Track completed processes
+
+    // Enqueue processes in order of arrival
+    for (int i = 0; i < num_processes; ++i) {
+        p[i].start_time = -1;  // -1 indicates the process hasn't started yet
+        process_queue.push(&p[i]);
+    }
+
+    while (completed_count < num_processes) {
+        if (process_queue.empty()) {
+            // If no processes are ready, move time forward
+            current_time++;
+            for (int i = 0; i < num_processes; ++i) {
+                if (!p[i].completed && p[i].arrival_time <= current_time) {
+                    process_queue.push(&p[i]);
+                }
+            }
+            
+            continue;
+        }
+
+        // Get the next process from the queue
+        Process* current_process = process_queue.front();
+        process_queue.pop();
+
+        // If the process hasn't started yet, set its start time
+        if (current_process->start_time == -1) {
+            current_process->start_time = current_time;
+        }
+
+        // Execute the process for the time quantum or remaining service time
+        int time_to_run = min(quantum, current_process->service_time);
+        current_process->service_time -= time_to_run;
+        current_time = (current_time +time_to_run);
+        // Mark the timeline with the process execution (for each time slice)
+        for (int t = current_time - time_to_run; t < current_time; ++t) {
+            timeline[t] = current_process->name;  // Use the first character of the process name
+        }
+
+
+        // If the process is completed, calculate stats
+        if (current_process->service_time == 0) {
+
+            current_process->completed = true;
+            completed_count++;
+            current_process->end_time = current_time -1;
+            cout << "Process: " << current_process->name
+                 << ", Start Time: " << current_process->start_time
+                 << ", End Time: " << current_process->end_time << endl;
+
+
+            // Update stats
+            current_process->stats.finish_time = current_process->end_time;
+            current_process->stats.turnaround = current_process->stats.finish_time - current_process->arrival_time;
+            current_process->stats.normturn = current_process->stats.turnaround / (float)(current_process->end_time - current_process->start_time);
+        } else {
+            // If the process is not finished, put it back in the queue
+            process_queue.push(current_process);
+        }
+
+        // Add newly arrived processes to the queue
+        for (int i = 0; i < num_processes; ++i) {
+            if (!p[i].completed && p[i].arrival_time <= current_time) {
+                bool already_in_queue = false;
+
+                // Check if the process is already in the queue
+                queue<Process*> temp_queue = process_queue;
+                while (!temp_queue.empty()) {
+                    if (temp_queue.front() == &p[i]) {
+                        already_in_queue = true;
+                        break;
+                    }
+                    temp_queue.pop();
+                }
+
+                // Enqueue if not already in queue
+                if (!already_in_queue) {
+                    process_queue.push(&p[i]);
+                }
+            }
+        }
+    }
+    for(int t=0;t<=process_time;t++){
+        printf("%c",timeline[t]);
+    }
+    printf("\n");
+    //print_trace(p, num_processes, process_time, "RR-" + to_string(quantum),timeline);
+}
+
+    // Print trace output (timeline)
+   
+    
+
+
+
+
 void spn(Process p[], int num_processes) {
     int current_time = 0;
     int completed_count = 0;
@@ -102,7 +229,9 @@ void spn(Process p[], int num_processes) {
             current_time++;
         }
     }
+   // print_trace(p, num_processes, 21, "SPN");
 }
+
 
 int main() {
 
@@ -164,8 +293,14 @@ getchar();
         processes[i].stats.normturn = 0.0;
         processes[i].completed=false;
     }
+    //if(strcmp(mode,"trace")==0){
+       //print_trace(processes,num_processes,21,"RR-4");
+    //}
 
- spn(processes, num_processes);
+
+ //spn(processes, num_processes);
+ round_robin(processes,timeline,num_processes,policy.quantum);
+
 // TEST FCFS
   /*     cout << "FCFS Scheduling:" << endl;
     fcfs(processes, num_processes);
